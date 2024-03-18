326ca7599bfa3fb7100a6e8d6c34242b56b8c9ff
Jan Schulte <janschul@cisco.com>
2024-03-18 16:34:01 -0400

    Initial Commit

---
+++ script.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "vulnerable_instance" {
+  ami           = "ami-12345678" # Outdated AMI with known vulnerabilities
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "VulnerableInstance"
+  }
+}
+
+resource "aws_security_group" "vulnerable_sg" {
+  name        = "vulnerable_sg"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_s3_bucket" "vulnerable_bucket" {
+  bucket = "my-vulnerable-bucket"
+  acl    = "public-read" # Publicly accessible
+
+  versioning {
+    enabled = false
+  }
+}
+
+resource "aws_db_instance" "vulnerable_db_instance" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  db_name              = "mydb"
+  username             = "user"
+  password             = "password" # Hardcoded secret
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+
+# Note: Replace the AMI ID with an outdated one specific to your region. The above ID is a placeholder.
